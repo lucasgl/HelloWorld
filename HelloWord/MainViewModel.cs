62062c6cbd653b3fdedb2d4fcbe75533c45ff249
JanaLocatelli <janainaribeirosc@gmail.com>
2016-04-24 20:39:17 -0400

    inserting mvvm!

---
+++ HelloWord/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWord
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if(handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private String txText = "Hello MVVM World!";
+
+        public String TxText
+        {
+            get { return txText; }
+            set
+            {
+                txText = value;
+                OnPropertyChanged("TxText");
+            }
+        }
+
+    }
+}
